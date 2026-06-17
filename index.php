7978a84f142f0d2068545fe2486baf63f1b305ee
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2026-06-17 16:52:40 +0700

    membuat setup env dan root awal project

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * ============================================================
+ * Sistem Perpustakaan Digital
+ * Arsitektur: MVC (Model-View-Controller)
+ * Stack: PHP Pure + MySQL + Tailwind CSS
+ * ============================================================
+ */
+
+require_once __DIR__ . '/config/app.php';
